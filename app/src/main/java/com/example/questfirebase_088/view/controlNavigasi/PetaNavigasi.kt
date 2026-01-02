dd9feaee2235a12aa771eecdd6d3f8b87a20b9e6
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2026-01-02 20:26:59 +0700

    menambahkan import

--- app/src/main/java/com/example/questfirebase_088/view/controlNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/questfirebase_088/view/controlNavigasi/PetaNavigasi.kt
@@ -6,7 +6,8 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.questfirebase_088.view.route.DestinasiDetail
+import com.example.questfirebase_088.view.EntrySiswaScreen
+import com.example.questfirebase_088.view.HomeScreen
 import com.example.questfirebase_088.view.route.DestinasiEntry
 import com.example.questfirebase_088.view.route.DestinasiHome
 
@@ -33,7 +34,7 @@ fun HostNavigasi(
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
                 navigateToItemUpdate = {
                     // Navigasi ke detail dengan membawa argumen ID
-                    navController.navigate("${DestinasiDetail.route}/$it")
+                    //navController.navigate("${DestinasiDetail.route}/$it")
                 }
             )
         }
