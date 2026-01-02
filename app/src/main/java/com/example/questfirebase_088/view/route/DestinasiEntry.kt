d65f3071c6200c5fd2145566f1fa0d0e28698dd0
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2026-01-02 19:52:53 +0700

    menambahkan fungsi DestinasiEntry.kt

--- app/src/main/java/com/example/questfirebase_088/view/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/questfirebase_088/view/route/DestinasiEntry.kt
@@ -1,2 +1,8 @@
 package com.example.questfirebase_088.view.route
 
+import com.example.questfirebase_088.R
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
+}
