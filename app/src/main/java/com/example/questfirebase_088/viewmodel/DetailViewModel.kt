8b1bbbf1f975e28d1edd455e1d79970340e7653f
dzakiyahalzahrani <dzakiyahalzahrani@gmail.com>
2026-01-08 21:48:44 +0700

    menambahkan file

--- app/src/main/java/com/example/questfirebase_088/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questfirebase_088/viewmodel/DetailViewModel.kt
@@ -1,2 +1,63 @@
+
 package com.example.questfirebase_088.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.questfirebase_088.modeldata.Siswa
+import com.example.questfirebase_088.repositori.RepositorySiswa
+import com.example.questfirebase_088.view.route.DestinasiDetail
+import kotlinx.coroutines.launch
+import java.io.IOException
+
+
+
+sealed interface StatusUIDetail {
+    data class Success(val statusiswa: Siswa?) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
+class DetailViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repositorySiswa: RepositorySiswa
+) : ViewModel() {
+
+    private val idSiswa: Long =
+        savedStateHandle.get<String>(DestinasiDetail.itemIdArg)?.toLong()
+            ?: error("idSiswa tidak ditemukan di SavedStateHandle")
+
+    var statusUIDetail: StatusUIDetail by mutableStateOf(StatusUIDetail.Loading)
+        private set
+
+    init {
+        getSatuSiswa()
+    }
+
+    fun getSatuSiswa() {
+        viewModelScope.launch {
+            statusUIDetail = StatusUIDetail.Loading
+            statusUIDetail = try {
+                StatusUIDetail.Success(
+                    statusiswa = repositorySiswa.getSatuSiswa(idSiswa)
+                )
+            } catch (e: IOException) {
+                StatusUIDetail.Error
+            } catch (e: Exception) {
+                StatusUIDetail.Error
+            }
+        }
+    }
+
+    suspend fun hapusSatuSiswa() {
+        try {
+            repositorySiswa.hapusSatuSiswa(idSiswa)
+            println("Sukses Hapus Data: $idSiswa")
+        } catch (e: Exception) {
+            println("Gagal Hapus Data: ${e.message}")
+        }
+    }
+}
